cedfdbd8c9b13e375d01d7178ed00f4aa4158736
Jasmine Walters <jsmnwltrs@gmail.com>
2018-11-13 21:22:53 -0600

    got hide and show to work with login

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import apiKeys from '../db/apiKeys.json';
 import './index.scss';
 import createNavbar from './component/Navbar/navbar';
 import loginButton from './component/Auth/auth';
+import checkLoginStatus from './helpers/authHelpers';
 
 const initializeApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   createNavbar();
+  checkLoginStatus();
   loginButton();
 };
 
