3c6125fbdc94b3cd16795a103e932b667e528b17
Jasmine Walters <jsmnwltrs@gmail.com>
2018-11-13 18:59:03 -0600

    added navbar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import $ from 'jquery';
 import 'bootstrap';
 import './index.scss';
 
-$('#button').click(() => {
-  $('#div').append('<p>Hey there!</p>');
-});
+import createNavbar from './component/navbar';
+
+const initializeApp = () => {
+  createNavbar();
+};
+
+initializeApp();
