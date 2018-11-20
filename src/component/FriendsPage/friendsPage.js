914c3e9db8b8251114683925334047481f7e6486
Jasmine Walters <jsmnwltrs@gmail.com>
2018-11-19 20:12:09 -0600

    got single friend printing

--- src/component/FriendsPage/friendsPage.js
+++ src/component/FriendsPage/friendsPage.js
@@ -3,6 +3,33 @@ import axios from 'axios';
 import apiKeys from '../../../db/apiKeys.json';
 import authHelpers from '../../helpers/authHelpers';
 
+const printSingleFriend = (friend) => {
+  const friendString = `
+  <div>
+    <h1>${friend.name}</h1>
+    <h3>${friend.relationship}</h3>
+    <p>${friend.phoneNumber}</p>
+    <p>${friend.address}</p>
+    <p>${friend.email}</p>
+    <button class="btn btn-danger delete-btn">X</button>
+  </div>`;
+  $('#single-container').html(friendString);
+};
+
+const getSingleFriend = (e) => {
+  // firebase id
+  const friendId = e.target.dataset.dropdownId;
+  axios.get(`${apiKeys.firebaseKeys.databaseURL}/friends/${friendId}.json`)
+    .then((result) => {
+      const singleFriend = result.data;
+      singleFriend.id = friendId;
+      printSingleFriend(singleFriend);
+    })
+    .catch((error) => {
+      console.error('getSingleFriend', error);
+    });
+};
+
 const buildDropdown = (friendsArray) => {
   let dropdown = `<div class="dropdown">
   <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -10,7 +37,7 @@ const buildDropdown = (friendsArray) => {
   </button>
   <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">`;
   friendsArray.forEach((friend) => {
-    dropdown += `<div class="dropdown-item">${friend.name}</div>`;
+    dropdown += `<div class="dropdown-item" data-dropdown-id=${friend.id}>${friend.name}</div>`;
   });
   dropdown += '</div></div>';
   $('#dropdown-container').html(dropdown);
@@ -35,4 +62,13 @@ const friendsPage = () => {
     });
 };
 
-export default friendsPage;
+const bindEvents = () => {
+  $('body').on('click', '.dropdown-item', getSingleFriend);
+};
+
+const initializeFriendsPage = () => {
+  friendsPage();
+  bindEvents();
+};
+
+export default initializeFriendsPage;
